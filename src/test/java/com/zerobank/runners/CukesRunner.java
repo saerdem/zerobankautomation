0cc337bc9d038d297c452cb716c0f50a019df8c8
saerdem <saerdem@gmail.com>
2020-06-18 00:21:54 +0300

    ali-2020.06.18

--- src/test/java/com/zerobank/runners/CukesRunner.java
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features/",
         glue = "com/zerobank/stepdefinitions",
         dryRun = false,
-        tags = "@description"
+        tags = "@findTransaction"
 
 )
 public class CukesRunner {
